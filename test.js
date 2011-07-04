930a4e2a732b5789080964f3e914045fc9959ec0
Stephen Searles <stephen.searles@gmail.com>
2011-07-04 00:44:52 -0700

    added a console check to see if undo is being registered

--- test.js
+++ test.js
@@ -1,5 +1,9 @@
 $(document).ready(function () {
-  var undo = new UndoManager();
+  var undo = new UndoManager({
+    undoChange:function () {
+      console.log("Undo registered");
+    }
+  });
   $('#testDiv').editable(function (value) {
     console.log("Test div is '" + value + "'.");
     return value;
