2530c4211fef46c8fc90b7be4c499a70ff909687
Stephen Searles <stephen.searles@gmail.com>
2011-07-04 01:01:21 -0700

    moved more toward plugin frameworks

--- jquery.jeditable.jsundoable.js
+++ jquery.jeditable.jsundoable.js
@@ -1,9 +1,22 @@
-var jsundoableplugin = function (settings,self) {
-  var form = this;
-  undo.undoable('undo change',function () {
-    form.find('input').each(function () {
-      this.val(this.val());
-    });
-  form.submit();
+var undo = new UndoManager({
+    undoChange:function () {
+      console.log("Undo registered");
+    }
   });
-}
+$.extend(jQuery,new UndoManager({
+  undoChange:function() {
+    console.log("Undo registered.");
+  }
+}));
+$.extend($.editable.types.text,{
+  plugin:function (settings,self) {
+    console.log("plugin applied");
+    var form = this;
+    undo.undoable('undo change',function () {
+      form.find('input').each(function () {
+        this.val(this.val());
+      });
+      form.submit();
+    });
+  }
+});
--- test.js
+++ test.js
@@ -1,14 +1,7 @@
 $(document).ready(function () {
-  var undo = new UndoManager({
-    undoChange:function () {
-      console.log("Undo registered");
-    }
-  });
   $('#testDiv').editable(function (value) {
     console.log("Test div is '" + value + "'.");
     return value;
-  },{
-  plugin:jsundoableplugin
   });
   $('#undoButton').click(function () {
     undo.undo();
