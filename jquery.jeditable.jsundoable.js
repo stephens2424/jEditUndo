8efd41c9bd503ce855182ff9a155e93e54474645
Stephen Searles <stephen.searles@gmail.com>
2011-07-05 11:48:01 -0700

    rewrote undo functions.

--- jquery.jeditable.jsundoable.js
+++ jquery.jeditable.jsundoable.js
@@ -4,24 +4,45 @@ jQuery.undoManager = new UndoManager({
     console.log("Undo stack changed.");
   }
 });
+//add default callback to jeditable
+jQuery.fn.editable.defaults.onsubmit = function(settings,element) {
+  var form = this;
+  var submitFunction;
+  $.each(form.data('events'),function (i,events){
+    $.each(events,function(i,event) {
+      if (event.type === 'submit') {
+        submitFunction = event.handler;
+      }
+    });
+  });
+  $.undoManager.undoable('undo name',function() {
+    return $.editable.types.text.undoFunction;
+  }(),[submitFunction,form,settings]);
+}
 //extend textbox type of jeditable to handle undo
 $.extend($.editable.types.text,{
-  plugin:function (settings,self) {
-    var form = this;
-    $.undoManager.undoable('undo change',function() {
-      var functionArray = new Array();
-      form.find('input').each(function () {
-        var originalText = $(this).val();
-        functionArray.push(function() {
-          $(self).text(originalText);
-        });
+  plugin:function(settings,self) {
+    var $element = $(self);
+    var $form = this;
+    var functionArray = new Array();
+    $form.find('input').each(function (i,input) {
+      var originalText = $(input).val();
+      functionArray.push(function() {
+        $element.text(originalText);
+        $(input).val(originalText);
       });
-      return function () {
-        for (var i in functionArray) {
-          functionArray[i].apply(form);
-        }
+    });
+    $.editable.types.text.undoFunction = function (submitFunction,form,settings) {
+      for (var i in functionArray) {
+        functionArray[i].apply($form);
       }
-    }());
+      $form.find('input').prop('type','hidden');
+      $form.submit(submitFunction);
+      $('body').append($form);
+      $form.triggerHandler('submit');
+      $form.remove();
+      console.log("I passed submit");
+    };
   }
 });
 //extend textarea type with the same plugin for text
