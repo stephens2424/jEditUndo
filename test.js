dcde83c4aed6d417cb69266e3f426b86ce840023
Stephen Searles <stephen.searles@gmail.com>
2011-07-07 08:47:57 -0700

    Added textarea test with js and style

--- test.js
+++ test.js
@@ -1,6 +1,12 @@
 $(document).ready(function () {
-  $('#testDiv').editable(function (value) {
+  $('#testDiv-text').editable(function (value) {
     return value;
+  });
+  $('#testDiv-textarea').editable(function (value) {
+    return value;
+  },{
+  type:"textarea",
+  submit:"save"
   });
   $('#undoButton').click(function () {
     $.undoManager.undo();
